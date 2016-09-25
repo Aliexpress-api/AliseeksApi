bfc118ffed6739224d0c2dba1bfa80664f02adf8
wakawaka54 <soue7@hotmail.com>
2016-09-25 16:40:30 -0500

    Fixing feedback bugs :c

--- src/AliseeksApi/Storage/Postgres/Feedback/FeedbackPostgres.cs
+++ src/AliseeksApi/Storage/Postgres/Feedback/FeedbackPostgres.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AliseeksApi.Models.Feedback;
 using Npgsql;
+using SharpRaven.Core;
+using AliseeksApi.Utility.Extensions;
 
 namespace AliseeksApi.Storage.Postgres.Feedback
 {
@@ -13,21 +15,29 @@ namespace AliseeksApi.Storage.Postgres.Feedback
         const string feedbackColumns = "message, email";
 
         private readonly IPostgresDb db;
+        private readonly IRavenClient raven;
 
-        public FeedbackPostgres(IPostgresDb db)
+        public FeedbackPostgres(IPostgresDb db, IRavenClient raven)
         {
             this.db = db;
         }
 
         public async Task InsertFeedback(FeedbackModel model)
-        { 
-            var command = new NpgsqlCommand();
-            var commandParameters = "@message, @email";
-            command.CommandText = $"INSERT INTO {feedbackTable} ({feedbackColumns}) VALUES ({commandParameters});";
-            command.Parameters.AddWithValue("@message", model.Message);
-            command.Parameters.AddWithValue("@email", model.Email);
+        {
+            try
+            {
+                var command = new NpgsqlCommand();
+                var commandParameters = "@message, @email";
+                command.CommandText = $"INSERT INTO {feedbackTable} ({feedbackColumns}) VALUES ({commandParameters});";
+                command.Parameters.AddWithValue("@message", model.Message);
+                command.Parameters.AddWithValue("@email", model.Email);
 
-            await db.CommandNonqueryAsync(command);
+                await db.CommandNonqueryAsync(command);
+            }
+            catch(Exception e)
+            {
+                await raven.CaptureNetCoreEventAsync(e);
+            }
         }
     }   
 }
