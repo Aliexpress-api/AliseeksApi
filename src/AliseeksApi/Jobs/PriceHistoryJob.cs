9806b25660ad3baf9eb34883fbad8e741fa799cb
Alex Bello <abello.2015@gmail.com>
2016-09-28 14:41:54 -0500

    Added price history job

---
+++ src/AliseeksApi/Jobs/PriceHistoryJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AliseeksApi.Models.Search;
+using AliseeksApi.Storage.Postgres.Search;
+
+namespace AliseeksApi.Jobs
+{
+    public class PriceHistoryJob
+    {
+        private readonly ISearchPostgres db;
+
+        public PriceHistoryJob(ISearchPostgres db)
+        {
+            this.db = db;
+        }
+
+        public void RunJob()
+        {
+            
+        }
+    }
+}
